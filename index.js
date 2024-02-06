6702475652b626cf7fecad01f48b0abd21df7d66
GAL\galha <galsherm@gmail.com>
2024-02-06 16:20:40 +0200

    Implement the post edit feature (there is still bug in the design of the editForm Page)

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 // Initialize an array to store blog posts
-const blogPosts = [];
+let blogPosts = [];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -29,12 +29,22 @@ app.post("/blog", (req, res) => {
   // Push the new post object into the array
   blogPosts.push(newPost);
 
-
+  //console.log("inside blog: ", blogPosts);
   res.render("index.ejs", { blogPosts: blogPosts });
-
 });
 
 
+// Endpoint to handle the editing of a post
+app.post("/editPost/:index", (req, res) => {
+  // Extract index and message from the request body
+  const index = req.params.index;
+
+  var post =  blogPosts[index];
+  // After updating the post, you can redirect the user to the home page or any other relevant page
+  res.render("form.ejs", { post:post });
+});
+
+// Endpoint to render the form for submitting a new post
 app.post("/submit", (req, res) => {
   res.render("form.ejs");
 });
